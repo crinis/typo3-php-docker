275bc5f339936cec3e2fe337b93ec6c6c06db233
crinis <code@crinis.org>
2025-01-27 19:24:53 +0100

    Set file perms for /var/www/html

--- 8.4/apache/entrypoint.sh
+++ 8.4/apache/entrypoint.sh
@@ -5,9 +5,10 @@
 set -euo pipefail
 
 if [ "$(id -u)" -eq 0 ]; then
-    # If SET_DOCROOT_OWNER is set to true, then change the UID and GID of the document root 
+    # If SET_OWNER is set to true, then change the UID and GID of the document root 
     # and its contents to www-data.
     if [ "${SET_OWNER}" == "true" ]; then
+        echo "Setting owner of document root and subdirectories to www-data."
         chown -R "www-data:www-data" /var/www/html
     fi
 
